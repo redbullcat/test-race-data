ff9d34105b5c7f41a5b8555c29aaf78acd1f15cb
redbullcat <philip.e.oakley@gmail.com>
2025-11-01 22:43:33 +0000

    Change y axis autorange to max

--- driver_pace_comparison_chart.py
+++ driver_pace_comparison_chart.py
@@ -94,6 +94,6 @@ def show_driver_pace_comparison(df, team_colors):
         legend_title="Percentile Range",
     )
 
-    fig.update_yaxes(autorange="min", range=y_range)
+    fig.update_yaxes(autorange="max", range=y_range)
 
     st.plotly_chart(fig, use_container_width=True)

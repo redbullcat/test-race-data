6c9b05ed723763034df9166156ce0c29520680fe
redbullcat <philip.e.oakley@gmail.com>
2025-11-07 12:22:30 +0000

    Add gap_evolution_chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from lap_position_chart import show_lap_position_chart
 from driver_pace_chart import show_driver_pace_chart
 from driver_pace_comparison_chart import show_driver_pace_comparison
 from results_table import show_results_table
+from gap_evolution_chart import show_gap_evolution_chart
 
 # --- Load available race data ---
 DATA_DIR = "data"
@@ -99,3 +100,4 @@ show_lap_position_chart(df, selected_cars, selected_classes, team_colors)
 show_driver_pace_chart(df, selected_cars, top_percent, selected_classes, team_colors)
 show_driver_pace_comparison(df, team_colors)
 show_results_table(df, team_colors)
+show_gap_evolution_chart(df, team_colors)
